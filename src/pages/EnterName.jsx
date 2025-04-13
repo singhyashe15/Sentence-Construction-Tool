cd39a319cf2290b469b06120addc6ee15ee3e7ff
singhyashe15 <singhyashraj574@gmail.com>
2025-04-13 16:29:03 +0530

    name page commited

--- src/pages/EnterName.jsx
+++ src/pages/EnterName.jsx
@@ -1,33 +1,31 @@
-import React, { useContext, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { UserContext } from "../context/UserContext.jsx";
+import { useUser } from "../context/UserContext.jsx";
 import toast from "react-hot-toast";
 
 const EnterName = () => {
-  const { name, setName } = useContext(UserContext); //storing the name in useContext Hook
-  const [inputName, setInputName] = useState(""); // Taking the input
+  const {name,setName } = useUser(); //storing the name in useContext Hook
   const navigate = useNavigate();
 
   // handle the submit buttton
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (inputName.trim() && inputName !== null) {
-      setName(inputName);
-      navigate("/home"); // Navigate to sentence quiz
+    if (name.trim() && name !== null) {
+      navigate("/display"); // Navigate to sentence quiz
     } else {
       toast.error("Enter your name first");
     }
   };
 
   return (
-    <div className="min-h-screen p-4 flex flex-col items-center justify-center overflow-hidden">
+    <div className="max-w-3xl mx-auto mt-16 p-4 flex flex-col items-center justify-center overflow-hidden">
       <h1 className="text-xl font-bold mb-4">Welcome to Sentence Construction Tool</h1>
       <div className="p-8 flex flex-col bg-white shadow-md rounded-lg text-center border border-gray-300 max-w-md w-full">
         <h2 className="text-xl font-bold mb-4">Enter Your Name</h2>
         <input //enter your name
           type="text"
-          value={inputName}
-          onChange={(e) => setInputName(e.target.value)}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           className="border p-2 rounded w-full"
           placeholder="Your name"
         />
