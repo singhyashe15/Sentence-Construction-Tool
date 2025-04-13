74b6bfc15d09968bd3afc0b29a468e722742b6ab
singhyashe15 <singhyashraj574@gmail.com>
2025-04-13 12:20:26 +0530

    dashboard page

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Dashboard(){
+  return(
+    <div className="min-h-screen p-4 flex flex-col items-center justify-center overflow-hidden">
+        
+    </div>
+  )
+}
