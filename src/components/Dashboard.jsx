20aa41a7c0ffea34e6fb1aacc5e4e8be6c4e9a3a
singhyashe15 <singhyashraj574@gmail.com>
2025-04-13 16:28:24 +0530

    change from component to pages

--- src/components/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export default function Dashboard(){
-  return(
-    <div className="min-h-screen p-4 flex flex-col items-center justify-center overflow-hidden">
-        
-    </div>
-  )
-}
